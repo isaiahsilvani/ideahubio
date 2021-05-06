6bb4fbc54382b8efef439893dd0c20039a5cde13
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-06 12:26:37 -0400

    apply authentication and conditional navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+      path('accounts/signup/', views.signup, name='signup'),
 ]
