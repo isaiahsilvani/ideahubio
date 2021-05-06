a715063b28de739bcb46e6b4d11dc9ffdf3a647c
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-06 12:40:54 -0400

    implement password reset

--- ideahubio/settings.py
+++ ideahubio/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ideahub.bot@gmail.com'
+EMAIL_HOST_PASSWORD = 'BotSoda420'
