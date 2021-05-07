bb270e4242b8674a1579a2cca31b5cb8cc56e139
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-07 13:18:00 -0400

    add logo

--- main_app/views.py
+++ main_app/views.py
@@ -66,6 +66,28 @@ def add_photo(request, idea_id):
   return redirect('detail', idea_id=idea_id)
 
 
+def add_logo(request, idea_id):
+  # get the idea, remove the old URL, replace it.
+  idea = Idea.objects.get(id=idea_id)
+  # photo-file will be the "name" attribute on the <input type="file">
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" for S3 / needs image file extension too
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    # just in case something goes wrong
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      # build the full url string
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      # we can assign to cat_id or cat (if you have a cat object)
+      idea.logo_url = url
+      idea.save()
+    except Exception as err:
+      print('An error occurred uploading file to S3: %s' % err)
+  return redirect('detail', idea_id=idea_id)  
+
+
 def signup(request):
   error_message = ''
   if request.method == 'POST':
