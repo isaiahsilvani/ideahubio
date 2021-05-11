5e0d6740ececa6e923a99bf4913eb8c879543b83
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-11 18:10:31 -0400

    deploy to heroku

--- ideahubio/settings.py
+++ ideahubio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,14 +56,17 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'ideahubio.urls'
 
 ASGI_APPLICATION = "ideahubio.asgi.application"
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
+
+
 
 TEMPLATES = [
     {
