c45e3e11d5e67907014283ff9105735575696a42
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-11 16:50:03 -0400

    deploy to heroku

--- ideahubio/settings.py
+++ ideahubio/settings.py
@@ -58,7 +58,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('https://ideahubio.herokuapp.com/', 6379)],
         },
     },
 }
