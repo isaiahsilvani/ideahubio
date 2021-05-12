b03712248a31f9cd48a0777dd8a94584e421b15c
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-11 20:04:13 -0400

    fix configuration

--- ideahubio/asgi.py
+++ ideahubio/asgi.py
@@ -8,8 +8,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
 import django
-
-from django.core.wsgi import get_wsgi_application
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
