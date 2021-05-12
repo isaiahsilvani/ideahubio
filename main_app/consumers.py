0144320f5aae0701a8ef6b0b3d2771aad585933c
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-12 11:31:19 -0400

    heroku

--- main_app/consumers.py
+++ main_app/consumers.py
@@ -43,13 +43,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event['message']
         username = event['username']
-        room = event['room']
-
-        Message.objects.create(
-            text=message,
-            room=room,
-            user=username
-        )
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
