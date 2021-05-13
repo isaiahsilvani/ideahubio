ce246317fd8486d03d996a03c6dd436bd0e1d9f3
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-13 16:55:05 -0400

    work

--- ideahubio/asgi.py
+++ ideahubio/asgi.py
@@ -19,7 +19,7 @@ from channels.routing import get_default_application
 import os
 
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ideahubio.settings')
 
 application = get_asgi_application()
