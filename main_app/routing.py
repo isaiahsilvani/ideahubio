ebb70cd35475a9d23dda9434ad9d420062ec1fa0
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-13 17:04:26 -0400

    fix

--- main_app/routing.py
+++ main_app/routing.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
 
 # Work!
