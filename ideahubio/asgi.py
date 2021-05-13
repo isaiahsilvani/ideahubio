7409371fc694107892766bea0559e90842ad0fd1
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-13 15:26:48 -0400

    heroku

--- ideahubio/asgi.py
+++ ideahubio/asgi.py
@@ -10,6 +10,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ideahubio.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ideahubio.settings')
 django.setup()
 application = get_default_application()
