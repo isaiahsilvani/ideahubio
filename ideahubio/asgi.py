bdbc0489fb9f4679c24fcf394c0d573d5d65931c
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-13 15:54:33 -0400

    heroku

--- ideahubio/asgi.py
+++ ideahubio/asgi.py
@@ -7,22 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import main_app.routing
-
-
 import django
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ideahubio.settings')
 django.setup()
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            main_app.routing.websocket_urlpatterns
-        )
-    )
-})
+application = get_default_application()
