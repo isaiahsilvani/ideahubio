162c1752f74eb8ff69724882a43215d236388eb6
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-13 16:52:25 -0400

    dont change

--- ideahubio/asgi.py
+++ ideahubio/asgi.py
@@ -16,13 +16,10 @@ import main_app.routing
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ideahubio.settings')
-django.setup()
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            main_app.routing.websocket_urlpatterns
-        )
-    )
-})
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = get_asgi_application()
